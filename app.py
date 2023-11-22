f52d54f3347128c1a7225bc7acd17bceb43e44d4
Martin Nguyen <nguyenquangduc2000@gmail.com>
2023-11-22 16:40:04 +0700

    Update API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, make_response
 from flask_cors import CORS
-from model import vi_pipeline
+from model import llm
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/llama/*": {"origins": "*"}})
@@ -9,27 +9,27 @@ cors = CORS(app, resources={r"/llama/*": {"origins": "*"}})
 def query():
     context = request.json.get('context', "")
     question = request.json.get('question', "")
-    lang = request.json.get('lang', "en")
+    lang = request.json.get('lang', "vi")
     
     if lang == "en":
-        output = vi_pipeline("<<SYS>> You are an intelligent agent who can answer questions based on context. <<SYS>>\n"
+        answer = llm(
         "Write an answer within 100 words for the question below based on the provided context. "
         "If the context provides insufficient information, reply \"I cannot answer\". "
         "Answer in an unbiased, comprehensive, and formal tone. "
         "If the question is subjective, provide an opinionated answer in the concluding 1-2 sentences.\n\n"
         f"Context: {context}\n"
         f"Question: {question}\n"
-        "Answer: ")
-        answer = output[0]["generated_text"]
+        "Answer: ") # "<<SYS>> You are an intelligent agent who can answer questions based on context. <<SYS>>\n"
+        # answer = output[0]["generated_text"]
     elif lang == "vi":
-        output = vi_pipeline("<<SYS>> Bạn là agent thông minh có thể trả lời câu hỏi theo ngữ cảnh. <<SYS>>\n"
+        answer = llm(
         "Hãy trả lời câu hỏi bên dưới với các thông tin được cung cấp trong phần ngữ cảnh. "
         "Nếu trong ngữ cảnh không có đủ thông tin, hãy trả lời \"Tôi không biết\". "
         "Câu trả lời phải đầy đủ thông tin, có giải thích và không nhiều hơn 100 từ.\n\n"
-        f"Ngữ cảnh: {context}\n"
-        f"Câu hỏi: {question}\n"
-        "Trả lời: ")
-        answer = output[0]["generated_text"]
+        f"Ngữ cảnh: {context}\n\n"
+        f"Câu hỏi: {question}\n\n"
+        "Trả lời: ") # "<<SYS>> Bạn là agent thông minh có thể trả lời câu hỏi theo ngữ cảnh. <<SYS>>\n"
+        # answer = output[0]["generated_text"]
     else:
         answer = "I do not understand your question!"
         
@@ -43,9 +43,9 @@ def api():
     lang = request.json.get('lang', "en")
     
     if lang == "en":
-        answer = vi_pipeline(prompt)
+        answer = llm(prompt)
     elif lang == "vi":
-        answer = vi_pipeline(prompt)
+        answer = llm(prompt)
     
     return make_response({"answer": answer}, 200)
 
--- model.py
+++ model.py
@@ -1,25 +1,33 @@
 import os
 import torch
 import transformers
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 
 model_id = "ura-hcmut/ura-llama-7b"
 
 en_pipeline_kwargs={"temperature": 1.0, 
-                    "max_new_tokens": 250, 
+                    "max_new_tokens": 256, 
                     "top_k": 1, 
                     "repetition_penalty": 1.1}
-vi_pipeline_kwargs={"temperature": 1.0, 
-                    "max_new_tokens": 250, 
-                    "top_k": 1,
-                    "repetition_penalty": 1.1}
+vi_pipeline_kwargs={
+    "bos_token_id": 1,
+    "do_sample": True,
+    "eos_token_id": 2,
+    "max_new_tokens": 512,
+    "pad_token_id": 0,
+    "temperature": 1.0,
+    "top_p": 0.9,
+    "top_k": 3,
+    "repetition_penalty": 1.1
+}
 
 # set quantization configuration to load large model with less GPU memory
 # this requires the `bitsandbytes` library
 bnb_config = transformers.BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type='nf4',
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_compute_dtype=torch.bfloat16
+    bnb_4bit_use_double_quant=False,
+    bnb_4bit_compute_dtype=torch.float16
 )
 
 vi_model = transformers.AutoModelForCausalLM.from_pretrained(
@@ -35,17 +43,22 @@ vi_model.eval()
 
 tokenizer = transformers.AutoTokenizer.from_pretrained(
     model_id,
+    truncation=True, 
+    max_length=256,
     trust_remote_code=True,
     use_auth_token=True
 )
-tokenizer.pad_token = tokenizer.eos_token
 
 print(f"Model loaded")
-
 vi_pipeline = transformers.pipeline(
     model=vi_model, 
     tokenizer=tokenizer,
-    return_full_text=False,  # langchain expects the full text
+    return_full_text=True,  # langchain expects the full text
     task='text-generation',
     **vi_pipeline_kwargs
 )
+
+
+llm = HuggingFacePipeline(pipeline=vi_pipeline)
+llm.pipeline.tokenizer.return_token_type_ids = False
+llm.pipeline.tokenizer.pad_token = llm.pipeline.tokenizer.eos_token
